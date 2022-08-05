8b6e4034c18f3f74d7ccfc81048fb2b6c5c59a9a
Michael Van Dorth <michael@fostercommerce.com>
2022-08-05 16:46:31 +0200

    Setup default layout and header and footer components

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="bg-white">
+    <TheHeader />
+    <main class="mt-8 max-w-2xl mx-auto pb-16 px-4 sm:pb-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <Nuxt />
+    </main>
+    <TheFooter />
   </div>
 </template>
