de84a8630d143756b498e77daff318ad847b5df2
Kingsley Atanang <Kingo4luv@yahoo.com>
2022-08-16 23:08:36 +0100

    updated filters directory

--- config/app.php
+++ config/app.php
@@ -64,7 +64,7 @@ return [
             }
         }
         return [
-            'class' => \modules\fc\filter\Cors::class,
+            'class' => \modules\fc\filters\Cors::class,
             'cors' => [
                 'Origin' => $origins,
                 'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
