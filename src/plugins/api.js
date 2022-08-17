fc69a97a0106a65ddab545780d1cdd41665ebedb
clowerweb <chris@clowerweb.com>
2022-08-16 23:00:22 -0400

    update api.js to use POST for add to cart

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { stringify } from 'qs';
 
 /*
 import { stringify } from 'qs';
@@ -20,32 +21,34 @@ const api = ($config, store) => ({
 		formData[csrfToken.name] = csrfToken.token;
 
 		if (Array.isArray(postData)) {
-			for (const [idx, item] of postData.entries()) {
-				formData[`purchasables[${idx}][id]`] = item.id;
-				formData[`purchasables[${idx}][qty]`] = item.qty;
+			for (const item of postData.entries()) {
+				formData.qty = item.qty;
+				formData.purchasableId = item.id;
 			}
 		}
 
-		/*const { data } = await axios.post($config.baseURL, formData, {
-			headers: {
-				'X-CSRF-Token': csrfToken.token,
-				'Content-Type': 'application/json',
-				Accept: 'application/json',
-				'X-Requested-With': 'XMLHttpRequest',
-				HTTP_X_REQUESTED_WITH: 'XMLHttpRequest',
-			}
-		});*/
+		const { data } = axios.post($config.baseURL,
+			formData,
+			{
+				withCredentials: true,
+				headers: {
+					'X-Requested-With': 'XMLHttpRequest',
+					'Content-Type': 'application/json',
+					Accept: 'application/json',
 
-		const { data } = await axios.get(`${$config.baseURL}`, {
+				},
+			}
+		).then((response) => {
+			response.sendStatus(200);
+			console.log(response);
+		});
+
+		/*const { data } = await axios.get('', {
 			params: {
 				CRAFT_CSRF_TOKEN: `${csrfToken.token}`,
 				action: 'commerce/cart/load-cart'
-			},
-			headers: {
-				'Content-Type': 'application/json',
-				Accept: 'application/json'
 			}
-		});
+		});*/
 
 		console.log(data);
 		return data;
