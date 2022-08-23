e8a234c125f2138e9304fd9f840fcd68ce64f881
Michael Van Dorth <michael@fostercommerce.com>
2022-08-23 17:04:58 +0200

    Re-applying header and store method changes

--- src/components/global/TheHeader.vue
+++ src/components/global/TheHeader.vue
@@ -10,6 +10,7 @@
 		computed:{
 			...mapGetters({
 				cartItems: 'cart/getItems',
+				navItems: 'getPrimaryNav'
 			}),
 		},
 		methods: {
@@ -23,43 +24,30 @@
 <template>
   <div>
     <div class="relative z-40 lg:hidden" role="dialog" aria-modal="true">
-
       <!-- Mobile Menu Overlay -->
-      <transition name="fade">
-        <div v-show="menuOpen" class="fixed inset-0 bg-black bg-opacity-25"></div>
-      </transition>
+			<div v-show="menuOpen" class="fixed inset-0 bg-black bg-opacity-25"></div>
 
       <!-- Mobile Fly Out Menu -->
-      <transition>
-        <div v-show="menuOpen" class="fixed inset-0 flex z-40">
-          <div class="relative max-w-xs w-full bg-white shadow-xl pb-12 flex flex-col overflow-y-auto">
-            <!-- Mobile Menu Close button -->
-            <div class="px-4 pt-5 pb-2 flex">
-              <button type="button" class="-m-2 p-2 rounded-md inline-flex items-center justify-center text-gray-400" @click="toggleMenu">
-                <span class="sr-only">Close menu</span>
-                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-              </button>
-            </div>
-            <!-- Mobile Menu Links -->
-            <div class="border-t border-gray-200 py-6 px-4 space-y-6">
-							<div class="flow-root">
-								<nuxt-link to="/women" class="-m-2 p-2 block font-medium text-gray-900">Women</nuxt-link>
-							</div>
-							<div class="flow-root">
-								<nuxt-link to="/men" class="-m-2 p-2 block font-medium text-gray-900">Men</nuxt-link>
-							</div>
-              <div class="flow-root">
-								<nuxt-link to="/about" class="-m-2 p-2 block font-medium text-gray-900">About</nuxt-link>
-              </div>
-              <div class="flow-root">
-								<nuxt-link to="/about/stores" class="-m-2 p-2 block font-medium text-gray-900">Stores</nuxt-link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </transition>
+			<div v-show="menuOpen" class="fixed inset-0 flex z-40">
+				<div class="relative max-w-xs w-full bg-white shadow-xl pb-12 flex flex-col overflow-y-auto">
+					<!-- Mobile Menu Close button -->
+					<div class="px-4 pt-5 pb-2 flex">
+						<button type="button" class="-m-2 p-2 rounded-md inline-flex items-center justify-center text-gray-400" @click="toggleMenu">
+							<span class="sr-only">Close menu</span>
+							<svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+							</svg>
+						</button>
+					</div>
+					<!-- Mobile Menu Links -->
+					<div class="border-t border-gray-200 py-6 px-4 space-y-6">
+						<div v-for="(link, index) in navItems" :key="index" class="flow-root">
+							<nuxt-link v-if="link.type === 'internal'" :to="link.url" :target="link.target" class="-m-2 p-2 block font-medium text-gray-900">{{ link.label }}</nuxt-link>
+							<a v-else :href="link.url" :target="link.target" class="-m-2 p-2 block font-medium text-gray-900">{{ link.label }}</a>
+						</div>
+					</div>
+				</div>
+			</div>
     </div>
 
     <!-- Main Site Header -->
@@ -80,17 +68,17 @@
             <!-- Non-mobile only links -->
             <div class="hidden lg:flex-1 lg:block lg:self-stretch">
               <div class="h-full flex space-x-8">
-								<nuxt-link to="/women" class="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800">Women</nuxt-link>
-								<nuxt-link to="/men" class="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800">Men</nuxt-link>
-								<nuxt-link to="/about" class="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800">About</nuxt-link>
-								<nuxt-link to="/about/stores" class="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800">Stores</nuxt-link>
+								<template v-for="(link, index) in navItems">
+									<nuxt-link v-if="link.type === 'internal'" :key="index" :to="link.url" :target="link.target" class="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800">{{ link.label }}</nuxt-link>
+									<a v-else :key="index" :href="link.url" :target="link.target" class="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800">{{ link.label }}</a>
+								</template>
               </div>
             </div>
 
             <!-- Logo -->
             <nuxt-link to="/" class="flex">
               <span class="sr-only">Foster Clothing</span>
-              <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600" alt="">
+              <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600" alt="Foster Clothing Logo" />
             </nuxt-link>
 
             <!-- Auxiliary Links -->
@@ -109,7 +97,7 @@
                   <svg class="flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
                   </svg>
-                  <span class="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">{{cartItems.length}}</span>
+                  <span class="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">{{ cartItems.length }}</span>
                   <span class="sr-only">items in cart, view bag</span>
                 </nuxt-link>
               </div>
