43eb5c4de00ae01b9da119cbdeb65d1f951601a1
Kingsley Atanang <Kingo4luv@yahoo.com>
2022-08-24 22:35:20 +0100

    added v-model to qty

--- src/components/checkout/CheckoutLineItem.vue
+++ src/components/checkout/CheckoutLineItem.vue
@@ -45,6 +45,7 @@
 					<label for="quantity_5" class="sr-only">Quantity, {{lineItem.title}}</label>
 					<input
 						id="quantity_5"
+						v-model="quanity"
 						name="quantity_5"
 						class="block max-w-[64px] rounded-md border border-gray-300 py-1.5 text-base leading-5 font-medium text-gray-700 text-left shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
 						type="number"
