707c67153559b8ad7debe7ac0fea1b6a66fa9cb1
Michael Van Dorth <michael@fostercommerce.com>
2022-09-04 17:05:36 +0200

    ensures the redrect pathc has a forward slash

--- src/components/product/ProductAddToCart.vue
+++ src/components/product/ProductAddToCart.vue
@@ -29,7 +29,15 @@
 		computed: {
 			...mapGetters('cart', [
 				'getCartErrors'
-			])
+			]),
+			/** Returns the redirect path with a forward slash if it does not have one */
+			redirectPath() {
+				if (this.redirect) {
+					return this.redirect.startsWith('/') ? this.redirect : ('/' + this.redirect);
+				} else {
+					return null;
+				}
+			}
 		},
 		methods: {
 			...mapActions('cart', [
@@ -40,8 +48,8 @@
 				this.adding = true;
 				await this.addNewItem({ ...this.purchasable, qty: Number(this.qty) });
 				this.adding = false;
-				if (this.redirect && this.getCartErrors.length === 0) {
-					await this.$router.push(this.redirect);
+				if (this.redirectPath && this.getCartErrors.length === 0) {
+					await this.$router.push(this.redirectPath);
 				}
 			}
 		}
