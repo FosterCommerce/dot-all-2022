010cd32fb6627b3d0e90e8b0a7f5bc0b97db4413
Michael Van Dorth <michael@fostercommerce.com>
2022-09-21 19:43:17 +0200

    Send username and password along with session info request
    Let’s try passing the username and password along with the session request to see if that works

--- src/store/user.js
+++ src/store/user.js
@@ -14,6 +14,14 @@ export const state = () => ({
 	 */
 	isGuest: true,
 	/**
+	 * Username (saved from login attempts)
+	 */
+	username: 'admin',
+	/**
+	 * Password (saved from login attempts)
+	 */
+	password: 'password',
+	/**
 	 * The user's email addresses.
 	 */
 	email: '',
@@ -43,6 +51,15 @@ export const getters = {
 	getIsGuest(state) {
 		return state.isGuest;
 	},
+
+	getUsername(state) {
+		return state.username;
+	},
+
+	getPassword(state) {
+		return state.password;
+	},
+
 	/**
 	 * Get the users email.
 	 *
@@ -68,6 +85,12 @@ export const mutations = {
 	setIsGuest(state, payload) {
 		state.isGuest = payload;
 	},
+	setUsername(state, payload) {
+		state.username = payload;
+	},
+	setPassword(state, payload) {
+		state.password = payload;
+	},
 	setEmail(state, payload) {
 		state.email = payload;
 	},
@@ -83,18 +106,49 @@ export const actions = {
 	 *
 	 * @param {function} commit - Vuex commit method.
 	 * @param {function} dispatch - Vuex dispatch method
+	 * @param {function} getters - Vuex getter method
 	 */
-	async fetchSessionData({ commit, dispatch }) {
+	async fetchSessionData({ getters, commit, dispatch }) {
+		// If we have a username and password, lets use it to send along with the session request
+		let config = {};
+		if (getters.getUsername && getters.getPassword) {
+			config = {
+				auth: {
+					username: getters.getUsername,
+					password: getters.getPassword
+				},
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}
+		}
+
 		// Get the session data from Craft and set it into state
-		const sessionInfo = await this.$api.get('/actions/users/session-info');
+		const sessionInfo = await this.$api.get('/actions/users/session-info', config);
+
+		console.log("Session Info Fetched", sessionInfo);
+
+		// Set the CSRF token and isGuest values. Also set the checkout steps (for logged in users)
 		commit('setCsrfToken', sessionInfo.csrfTokenValue, { root: true });
 		commit('setIsGuest', sessionInfo.isGuest);
-		if (!sessionInfo.isGuest) {
-			await dispatch('fetchUser', sessionInfo.email);
-		}
 		dispatch('checkout/fetchSteps', sessionInfo.isGuest, { root: true });
+
+		if (!sessionInfo.isGuest) {
+			// If the user is not a guest, let's fetch their data
+			await dispatch('fetchUser', sessionInfo.email);
+		} else {
+			// They are a guest, so let's just reset the username and password to null
+			commit('setUsername', null);
+			commit('setPassword', null);
+		}
 	},
 
+	/**
+	 * Uses GraphQL to fetch the a users data from Craft and places it into state
+	 *
+	 * @param {function} commit - Vuex commit method.
+	 * @param {string} email - a users email address
+	 */
 	async fetchUser({ commit }, email) {
 		if (email) {
 			const { data } = await this.$api.graphqlQuery(
